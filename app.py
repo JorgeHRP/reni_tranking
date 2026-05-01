3d1f8abae1a5de8daf6d2b5913b999c3e808fe4d
Jorge Henrique <jorgeprado.contato@outlook.com>
2026-05-01 01:49:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def track(phone):
         
     return redirect(SITE_DESTINO)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
